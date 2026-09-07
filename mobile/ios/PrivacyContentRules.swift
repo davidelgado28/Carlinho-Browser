6c0b00f9b09f9c32ff5a89d9d5fc48a7b27e8334
David Carlos <davidcarlosdelgado28@gmail.com>
2026-09-07 13:40:42 -0300

    Implement PrivacyContentRules for WKWebView

--- mobile/ios/PrivacyContentRules.swift
+++ mobile/ios/PrivacyContentRules.swift
@@ -1,1 +1,18 @@
+import WebKit
 
+final class PrivacyContentRules {
+    static func attach(to configuration: WKWebViewConfiguration, completion: @escaping (Error?) -> Void) {
+        let rules = """
+        [
+          {"trigger":{"url-filter":".*doubleclick\\\\.net.*"},"action":{"type":"block"}},
+          {"trigger":{"url-filter":".*google-analytics\\\\.com.*"},"action":{"type":"block"}},
+          {"trigger":{"url-filter":".*googletagmanager\\\\.com.*"},"action":{"type":"block"}},
+          {"trigger":{"url-filter":".*facebook\\\\.net.*"},"action":{"type":"block"}}
+        ]
+        """
+        WKContentRuleListStore.default().compileContentRuleList(forIdentifier: "CarlinhoPrivacy", encodedContentRuleList: rules) { list,error in
+            guard let list else { completion(error); return }
+            configuration.userContentController.add(list); completion(nil)
+        }
+    }
+}
