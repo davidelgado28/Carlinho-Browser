63461a391a9ca9ec87b7828b60383036c27eaacb
David Carlos <davidcarlosdelgado28@gmail.com>
2026-09-07 13:47:22 -0300

    Delete desktop directory

--- desktop/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "browser_window.h"
-#include <QApplication>
-int main(int argc, char* argv[]) {
-    QApplication app(argc, argv);
-    app.setApplicationName("Carlinho Browser");
-    app.setApplicationVersion(qEnvironmentVariable("CARLINHO_VERSION", "0.1.0"));
-    BrowserWindow window; window.resize(1440,900); window.show();
-    return app.exec();
-}
